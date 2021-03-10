0a9425b1ca2b1224740f018b8fe1244b167fa29f
Lucas Sousa <frempyrean@gmail.com>
2021-03-10 20:41:35 -0300

    feat: add logic to middlewares

--- src/index.js
+++ src/index.js
@@ -10,19 +10,38 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers
+  const user = users.find((user => user.username === username))
+  if (!user) return response.status(404).json({ error: 'User not found' })
+  request.user = user
+  next()
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request
+  if (!user.pro && user.todos.length >= 10) return response.status(403).json({ error: 'Exceeded free quota' })
+  next()
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers
+  const { id } = request.params
+  const user = users.find((user) => user.username === username)
+  if (!user) return response.status(404).json({ error: 'Could not find user' })
+  if (!(validate(id))) return response.status(400).json({ error: 'Invalid id' })
+  const todo = user.todos.find((todo) => todo.id === id)
+  if (!todo) return response.status(404).json({ error: 'Could not find todo' })
+  request.user = user
+  request.todo = todo
+  next()
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params
+  const user = users.find((user) => user.id === id)
+  if (!user) return response.status(404).json({ error: 'User not found' })
+  request.user = user
+  next()
 }
 
 app.post('/users', (request, response) => {
